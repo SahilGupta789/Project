d922d39b8a90ed86768a3fa4ff26b8725e531cbe
SahilGupta789 <112715879+SahilGupta789@users.noreply.github.com>
2025-05-01 13:03:44 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = (
    cognito: (
      userPoolId: "us-east-1_RVm4OFjX3",
      userPoolClientId: "4h05v2td7qqq98daes09tjo1dr",
-     region: "s-east-1"
+     region: "us-east-1"
    ),
   api: (
     serviceUrl: ""
