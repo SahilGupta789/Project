3835a6257e1272689205aaf8100647a5f890618b
SahilGupta789 <112715879+SahilGupta789@users.noreply.github.com>
2025-05-01 13:00:26 +0530

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = (
+   cognito: (
+     userPoolId: "us-east-1_RVm4OFjX3",
+     userPoolClientId: "4h05v2td7qqq98daes09tjo1dr",
+     region: "s-east-1"
+   ),
+  api: (
+    serviceUrl: ""
+  )
+);
